35f93898545e6ce7eec9f5f0147a1701f47a1588
armaan2006-io <razervortex9@gmail.com>
2026-05-18 17:29:54 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const brokenVariable = "I am not being used anywhere!";
+
+function App() {
+  return (
+    <div>
+      <h1>Testing the Code Police!</h1>
+    </div>
+  );
+}
+
+export default App;
